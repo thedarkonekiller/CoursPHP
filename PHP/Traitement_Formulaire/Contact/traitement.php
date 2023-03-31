dec7c77036a90dbc3910f3ffad6e373549a31a1d
thedarkonekiller <76535453+thedarkonekiller@users.noreply.github.com>
2023-03-31 16:04:56 +0200

    maj

--- PHP/Traitement_Formulaire/Contact/traitement.php
+++ PHP/Traitement_Formulaire/Contact/traitement.php
@@ -8,11 +8,37 @@
 </head>
 <body>
     <?php var_dump($_POST) ?>
- <ul>   
-<?php foreach ($_POST as $value): ?>
-    <li><?= $value; ?></li>
-<?php endforeach; ?>
- </ul>
+<!-- Début phase de tests -->
+<?php 
+    if (!empty($_POST)) {
+        header('Location: ./contact.php');
+    }
+
+
+$pattern = "/^[a-zA-ZÀ-ÿ]+$/";
+$patternEmail = '/[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+.[a-zA-Z]{2,4}/igm'
+
+
+preg_match($pattern, trim($_GET["prenom"]))
+
+if(preg_match($pattern, trim($_GET["nom"])) && strlen($_GET["nom"]) > 1 && strlen($_GET["nom"]) < 26) {
+    echo $_GET["nom"] ;
+} else {
+    echo "Le champ nom n'est pas bien renseigné"
+}
+
+if(preg_match($pattern, trim($_GET["prenom"])) && strlen($_GET["prenom"]) > 1 && strlen($_GET["prenom"]) < 26) {
+    echo $_GET["prenom"] ;
+} else {
+    echo "Le champ prenom n'est pas bien renseigné"
+}
+
+filter_var(trim($_GET["email"]), FILTER_VALIDATE_EMAIL));
+
+
+
+
+?>
 </body>
 </html>
 
