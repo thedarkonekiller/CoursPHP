31178a16e8b4083ed9508a94543d2fcb543345a1
thedok02 <florian.charpentier02@gmail.com>
2023-04-09 10:58:07 +0200

    maj

--- PHP-SQL/admin_traitement.php
+++ PHP-SQL/admin_traitement.php
@@ -1,4 +1,4 @@
-<?php require_once './database.php' ?>
+<?php require_once './database.php'; ?>
 
 <?php
 $db = new PDO('mysql:host='.$host.';dbname='.$dbName.';charset='.$charset.';',$dbUser,$dbPwd);
